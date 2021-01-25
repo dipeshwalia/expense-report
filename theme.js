b421027298729f557fff4959c13af7ba5a86c684
dipesh walia <dipesh.walia@b-yond.com>
2021-01-25 17:21:24 -0500

    chore : add docker-compose

--- theme.js
+++ theme.js
@@ -1,6 +1,7 @@
+import React from 'react';
+
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
-
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
